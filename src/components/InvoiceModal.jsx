bbad8e4d6df4698b7483ce9ff3143247fc4fd4ac
SAMEERMAKWANA74 <sameermakwana74@gmail.com>
2025-12-10 16:10:36 +0530

    chages are done

--- src/components/InvoiceModal.jsx
+++ src/components/InvoiceModal.jsx
@@ -43,9 +43,7 @@ const InvoiceModal = ({
         <div id="invoiceCapture">
           <div className="d-flex flex-row justify-content-between align-items-start bg-light w-100 p-4">
             <div className="w-100">
-              <h4 className="fw-bold my-2">
-                {info.billFrom || "John Uberbacher"}
-              </h4>
+              <h4 className="fw-bold my-2">{info.billFrom || "John Uberbacher"}</h4>
               <h6 className="fw-bold text-secondary mb-1">
                 Invoice Number: {info.invoiceNumber || ""}
               </h6>
@@ -53,11 +51,11 @@ const InvoiceModal = ({
             <div className="text-end ms-4">
               <h6 className="fw-bold mt-1 mb-2">Amount&nbsp;Due:</h6>
               <h5 className="fw-bold text-secondary">
-                {" "}
                 {currency} {total}
               </h5>
             </div>
           </div>
+
           <div className="p-4">
             <Row className="mb-4">
               <Col md={4}>
@@ -77,6 +75,7 @@ const InvoiceModal = ({
                 <div>{info.dateOfIssue || ""}</div>
               </Col>
             </Row>
+
             <Table className="mb-0">
               <thead>
                 <tr>
@@ -86,79 +85,63 @@ const InvoiceModal = ({
                   <th className="text-end">AMOUNT</th>
                 </tr>
               </thead>
+
               <tbody>
-                {items.map((item, i) => {
-                  return (
-                    <tr id={i} key={i}>
-                      <td style={{ inline-size: "70px" }}>{item.quantity}</td>
-                      <td>
-                        {item.name} - {item.description}
-                      </td>
-                      <td className="text-end" style={{ inline-size: "100px" }}>
-                        {currency} {item.price}
-                      </td>
-                      <td className="text-end" style={{ inline-size: "100px" }}>
-                        {currency} {item.price * item.quantity}
-                      </td>
-                    </tr>
-                  );
-                })}
+                {items.map((item, i) => (
+                  <tr id={i} key={i}>
+                    <td style={{ inlineSize: "70px" }}>{item.quantity}</td>
+                    <td>{item.name} - {item.description}</td>
+                    <td className="text-end" style={{ inlineSize: "100px" }}>
+                      {currency} {item.price}
+                    </td>
+                    <td className="text-end" style={{ inlineSize: "100px" }}>
+                      {currency} {item.price * item.quantity}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
+
             <Table>
               <tbody>
-                <tr>
-                  <td>&nbsp;</td>
-                  <td>&nbsp;</td>
-                  <td>&nbsp;</td>
-                </tr>
+                <tr><td colSpan="3">&nbsp;</td></tr>
+
                 <tr className="text-end">
                   <td></td>
-                  <td className="fw-bold" style={{ inline-size: "100px" }}>
-                    SUBTOTAL
-                  </td>
-                  <td className="text-end" style={{ inline-size: "100px" }}>
-                    {currency} {subTotal}
-                  </td>
+                  <td className="fw-bold">SUBTOTAL</td>
+                  <td>{currency} {subTotal}</td>
                 </tr>
-                {taxAmount !== 0.0 && (
+
+                {taxAmount !== 0 && (
                   <tr className="text-end">
                     <td></td>
-                    <td className="fw-bold" style={{ inline-size: "100px" }}>
-                      TAX
-                    </td>
-                    <td className="text-end" style={{ inline-size: "100px" }}>
-                      {currency} {taxAmount}
-                    </td>
+                    <td className="fw-bold">TAX</td>
+                    <td>{currency} {taxAmount}</td>
                   </tr>
                 )}
-                {discountAmount !== 0.0 && (
+
+                {discountAmount !== 0 && (
                   <tr className="text-end">
                     <td></td>
-                    <td className="fw-bold" style={{ inline-size: "100px" }}>
-                      DISCOUNT
-                    </td>
-                    <td className="text-end" style={{ inline-size: "100px" }}>
-                      {currency} {discountAmount}
-                    </td>
+                    <td className="fw-bold">DISCOUNT</td>
+                    <td>{currency} {discountAmount}</td>
                   </tr>
                 )}
+
                 <tr className="text-end">
                   <td></td>
-                  <td className="fw-bold" style={{ inline-size: "100px" }}>
-                    TOTAL
-                  </td>
-                  <td className="text-end" style={{ inline-size: "100px" }}>
-                    {currency} {total}
-                  </td>
+                  <td className="fw-bold">TOTAL</td>
+                  <td>{currency} {total}</td>
                 </tr>
               </tbody>
             </Table>
+
             {info.notes && (
               <div className="bg-light py-3 px-4 rounded">{info.notes}</div>
             )}
           </div>
         </div>
+
         <div className="pb-4 px-4">
           <Row>
             <Col md={6}></Col>
@@ -169,7 +152,7 @@ const InvoiceModal = ({
                 onClick={GenerateInvoice}
               >
                 <BiCloudDownload
-                  style={{ inline-size: "16px", block-size: "16px", margininset-block-start: "-3px" }}
+                  style={{ inlineSize: "16px", blockSize: "16px" }}
                   className="me-2"
                 />
                 Download Copy
@@ -178,7 +161,6 @@ const InvoiceModal = ({
           </Row>
         </div>
       </Modal>
-      
     </div>
   );
 };
